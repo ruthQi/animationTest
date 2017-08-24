93ae755334b22fa53f2b5082deb515b8426bb823
qiru <bjqiru@corp.netease.com>
2017-08-24 12:13:46 +0800

    添加场景2

--- public/scripts/pages/h5/miss/miss-s.js
+++ public/scripts/pages/h5/miss/miss-s.js
@@ -12,6 +12,9 @@ class MissAnimation {
       this.birdArr = [];
       this.distance = 0;
       this.leaveFlag = false;
+      this.posFlag = true;//G
+      this.birdRotation = 0;//Y
+      this.curPos = 0;//Q
       this.loaderArray();
       this.init();
    }
@@ -346,15 +349,16 @@ class MissAnimation {
       this.renderScene1();
       this.renderBird();
       this.renderLeave();
+      this.renderTreeCover();
       //场景2
       this.renderScene2();
+      this.renderClock();
       this.container.addChild(this.mainScene);
       this.bindEvent();
       //必须设置此属性，滚动式才好使，否则滚动不起作用
       this.scroller.setDimensions(this.viewWidth, this.viewHeight, this.viewWidth, 29360);
       //this.scroller.scrollTo(0,0,false);
       this.updateLoop();
-      //this.canvasView.render(this.container);
    }
    bindEvent(){
       this.mainScene.interactive = true;
@@ -394,6 +398,12 @@ class MissAnimation {
 
       this.mainScene.position.x = -left;
       this.mainScene.position.y = -top;
+      if(top > this.curPos){
+         this.posFlag = true
+      }else if(top < this.curPos){
+         this.posFlag = false;
+      }
+      this.birdRotation = this.posFlag ? 0 : Math.PI;
       if(top < 3900){
          for(var i=0;i<this.sContainer1.children.length;i++){
             let item = this.sContainer1.children[i];
@@ -412,10 +422,11 @@ class MissAnimation {
             if(top < 1280){
                let x = Math.sqrt(256e4 - top * top) / 2, angle = Math.asin(top / 1600) + 0;
                this.birdImg.position.set(x + 65, 200 + 1.2 * top);
+               //rotation表示绕中心点旋转多少
                this.birdImg.rotation = angle;
             }else{
                this.birdImg.position.set(1280 - top + 480 + 65, 200 + 1.2 * top);
-               this.birdImg.rotation = Math.asin(.8) + 0;
+               this.birdImg.rotation = Math.asin(.8) + this.birdRotation;
             }
             if(this.leaveFlag){
                this.stopLeave();
@@ -423,11 +434,11 @@ class MissAnimation {
          }else if(top > 2600){
             if(top < 2800){
                this.birdImg.position.set(top - 2600, top + 600);
-               this.birdImg.rotation = -Math.PI / 4 + 0;
+               this.birdImg.rotation = -Math.PI / 4 + this.birdRotation;
             }else if(top < 3160){
                var height = 1600 - (top - 2800),
                   heightS = Math.sqrt(256e4 - height * height) / 9,
-                  heightC = Math.acos(height / 1600) + 0;
+                  heightC = Math.acos(height / 1600) + this.birdRotation;
                   this.birdImg.position.set(200 + heightS, top + top - 2800 + 600);
                   this.birdImg.rotation = -Math.PI / 4 + heightC;
             }else{
@@ -438,7 +449,70 @@ class MissAnimation {
             }
          }
       }
-
+      this.curPos = top;
+      if(top >= 3900 && top < 9e3){
+         for(var i=0;i<this.s2Container.children.length; i++){
+            let item = this.s2Container.children[i];
+            //console.log(item)
+            if(item.data.speed){
+               let dValue = top - item.data.position.y;
+               if(item.data.speed.x){
+                  let translateX = 0;
+                  if(item.data.translate){
+                     translateX = item.data.translate.x;
+                  }
+                  item.position.x = item.data.position.x + item.data.speed.x * dValue + translateX;
+               }
+               if(item.data.speed.y){
+                  let translateY = 0;
+                  if(item.data.translate){
+                     translateY = item.data.translate.y;
+                  }
+                  item.position.y = item.data.position.y + item.data.speed.y * dValue + translateY;
+               }
+               if(i == 0){
+                  this.clock.position.set(326, 5273 + item.data.speed.y * dValue);
+               }
+            }
+            if(item.data.animate){
+               if(i == 9){
+                  if(top > 6650 && top <= 6750){
+                     item.gotoAndStop(1);
+                     this.plane1.position.set(360, 7260);
+                     this.plane2.position.set(360, 7260);
+                  }else if(top > 6750 && top < 7200){
+                     item.gotoAndStop(0);
+                     this.plane1.visible = true;
+                     this.plane2.visible = false;
+                     this.plane1.position.set(item.position.x + 105, 7120);
+                  }else if(top <= 6650){
+                     item.gotoAndStop(0);
+                     this.plane1.position.set(360, 7260);
+                     this.plane2.position.set(360, 7260);
+                     this.plane1.visible = false;
+                     this.plane2.visible = true;
+                  }
+               }
+               if(i == 11){
+                  let dValue = top - item.data.position.y;
+                  item.gotoAndStop(parseInt(dValue / 15));
+               }
+            }
+         }
+         if(top > 5200 && top < 6100){
+            let dV = top - 5200;
+            this.plane1.position.set(802-dV, 5600+1.2*dV);
+            this.plane2.position.set(802-dV, 5600+1.2*dV);
+            this.plane1.visible = this.posFlag;
+            this.plane2.visible = !this.posFlag;
+         }else if (top >= 6100 && top < 6500) {
+            let dV = top - 6200;
+            this.plane1.position.set(1.2*dV, 6900 + 1.2*dV);
+            this.plane2.position.set(1.2*dV, 6900 + 1.2*dV);
+            this.plane1.visible = !this.posFlag;
+            this.plane2.visible = this.posFlag;
+         }
+      }
    }
    stopLeave(){
       this.leaveFlag = false;
@@ -481,6 +555,7 @@ class MissAnimation {
    renderBird(){
       this.birdImg = new PIXI.extras.AnimatedSprite.fromImages(this.birdArr);
       this.birdImg.animationSpeed = 0.15;
+      //表示设置this.birdImg的中心点为115,145；
       this.birdImg.pivot.set(115, 145);
       this.birdImg.play();
       this.mainScene.addChild(this.birdImg);
@@ -494,9 +569,62 @@ class MissAnimation {
       }
       this.mainScene.addChild(this.leaveContainer);
    }
+   renderTreeCover(){
+      let treeCover = new PIXI.Sprite(this.loader.resources[this.imgSrc+'tree_cover.png'].texture);
+      treeCover.position.set(92, 3945);
+      this.mainScene.addChild(treeCover);
+   }
    //L
    renderScene2(){
-
+      this.s2Container = new PIXI.Container();
+      let sImage;
+      for(var i=0; i<this.scene2.length; i++){
+         if(this.scene2[i].animate){
+            sImage = new PIXI.extras.AnimatedSprite.fromImages(this.scene2[i].animateData);
+            sImage.animationSpeed = this.scene2[i].animationSpeed;
+         }else{
+            sImage = new PIXI.Sprite(this.loader.resources[this.scene2[i].url].texture);
+         }
+         sImage.data = this.scene2[i];
+         sImage.position.set(this.scene2[i].position.x, this.scene2[i].position.y);
+         if(i == 7){
+            this.renderPlane();
+         }
+         this.s2Container.addChild(sImage);
+      }
+      this.mainScene.addChild(this.s2Container);
+   }
+   renderPlane(){
+      this.plane1 = new PIXI.Sprite(this.loader.resources[this.imgSrc+'plane1.png'].texture);
+      this.plane1.position.set(802, 5998);
+      this.plane2 = new PIXI.Sprite(this.loader.resources[this.imgSrc+'plane2.png'].texture);
+      this.plane2.position.set(802, 5998);
+      this.plane1.data = {speed: 0};
+      this.plane2.data = {speed: 0};
+      this.s2Container.addChild(this.plane1, this.plane2);
+   }
+   renderClock(){
+      this.clock = new PIXI.Container();
+      this.clock.position.set(326, 5273);
+      let hClock = new PIXI.Sprite(this.loader.resources[this.imgSrc+'clock/h.png'].texture);
+      hClock.pivot.set(9, 35);
+      hClock.position.set(50, 51);
+      let mClock = new PIXI.Sprite(this.loader.resources[this.imgSrc+'clock/m.png'].texture);
+      mClock.pivot.set(8, 42);
+      mClock.position.set(50, 51);
+      let sClock = new PIXI.Sprite(this.loader.resources[this.imgSrc+'clock/s.png'].texture);
+      sClock.pivot.set(4, 42);
+      sClock.position.set(50, 51);
+      setInterval(()=>{
+         let hour = new Date().getHours();
+         let minute = new Date().getMinutes();
+         let second = new Date().getSeconds();
+         hClock.rotation = hour >= 12 ? (hour - 12 + minute/60)/12 * (2* Math.PI) : (hour + minute/60)/12*(2*Math.PI);
+         mClock.rotation = (minute + second/60)/60 * (2*Math.PI);
+         sClock.rotation = second/60 * 2* Math.PI;
+      },1e3)
+      this.clock.addChild(hClock, mClock, sClock);
+      this.mainScene.addChild(this.clock);
    }
 }
 
